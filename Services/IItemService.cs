efed53fdeac8459831259741502538076f517fc8
mawadasweis19 <67907899+mawada-sweis@users.noreply.github.com>
2024-04-04 20:55:59 +0300

    solve item service interface error

--- Services/IItemService.cs
+++ Services/IItemService.cs
@@ -11,7 +11,14 @@ namespace InventoryManagementSystem.Services
     public interface IItemService
     {
         Item GetItem();
-        void AddItem();
+        void AddItem(string name,
+            string description,
+            int price,
+            string status,
+            int quantity,
+            int minQuantity,
+            int sold = 0
+            );
         bool UpdateMessage();
         bool DeleteItem(Guid id);
     }
